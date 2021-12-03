ca9a6fcdbcebe6c8ed2d8ed92281c9d99eca753e
fsdexter <fsdexter@gmail.com>
2021-12-03 10:16:12 +0100

    __str__ implementede

--- reto.py
+++ reto.py
@@ -15,6 +15,10 @@ class Empleado:
         Empleado.available_id += 1
 
 
+    def __str__(self):
+        return f'El empleado {self.name} {self.surname} cobra {self.pay}'
+
+
     def pay_rise(self):
         self.pay = int(self.pay * 1.15)
 
@@ -39,11 +43,16 @@ emp_2 = Empleado('Miguel', 'Betegon')
 emp_3 = Empleado('Enrique', 'Pernia')
 Jaled = Empleado.from_string("Jaled, Moustafa")
 
+print(emp_1)
+print(emp_2)
+print(emp_3)
+print(Jaled)
 
-print(emp_1.name, emp_1.surname,emp_1.email,emp_1.pay, emp_1.id)
-print(emp_2.name, emp_2.surname,emp_2.email,emp_2.pay, emp_2.id)
-print(emp_3.name, emp_3.surname,emp_3.email,emp_3.pay, emp_3.id)
-print(Jaled.name, Jaled.surname,Jaled.email,Jaled.pay, Jaled.id)
+
+# print(emp_1.name, emp_1.surname,emp_1.email,emp_1.pay, emp_1.id)
+# print(emp_2.name, emp_2.surname,emp_2.email,emp_2.pay, emp_2.id)
+# print(emp_3.name, emp_3.surname,emp_3.email,emp_3.pay, emp_3.id)
+# print(Jaled.name, Jaled.surname,Jaled.email,Jaled.pay, Jaled.id)
 # emp_1.pay_rise()
 # print(emp_1.pay)
 # emp_2.pay_rise()
